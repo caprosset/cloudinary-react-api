c2132b90f48d90e6541c8130c19de512da7577a1
caprosset <capucinerosset@gmail.com>
2021-03-15 22:15:15 +0100

    Upload app skeleton

---
+++ routes/project.routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const fileParser = require('../configs/cloudinary-setup.config');
+const Project = require('../models/Project.model');
+
+router.get('/projects', (req, res, next) => {
+  Project.find()
+    .then(projectsFromDB => res.status(200).json(projectsFromDB))
+    .catch(err => next(err));
+});
+
+router.post('/projects/create', (req, res, next) => {
+  // console.log('req.body', req.body)
+  Project.create(req.body)
+    .then(aNewproject => {
+      // console.log('Created new project: ', aNewproject);
+      res.status(200).json(aNewproject);
+    })
+    .catch(err => next(err));
+});
+
+router.post('/projects/upload', fileParser.single('image'), (req, res, next) => {
+  console.log('req.file', req.file);
+  if(!req.file) {
+    next(new Error('No file uploaded'));
+    return;
+  }
+  res.json(req.file.path);
+})
+
+module.exports = router;
